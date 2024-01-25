ae1f34ec2b9e3f66e73e08406493046f8efbfa40
Basile Bartolomeo <basilebartolomeo01@gmail.com>
2024-01-25 13:10:19 +0100

    finished exercise

--- src/GetterAndSetter.java
+++ src/GetterAndSetter.java
@@ -1,5 +1,22 @@
 public class GetterAndSetter {
-    Studente studente1 = new Studente("Carlo", "Diquattro", 123456);
+    public static void main(String[] args) {
+        Studente studente1 = new Studente("Carlo", "Diquattro", 123456);
 
+        // Print infos
+        System.out.println("Name: " + studente1.getName());
+        System.out.println("Surname: " + studente1.getSurname());
+        System.out.println("Id: " + studente1.getId());
 
+        // Modify infos
+        studente1.setName("Roberto");
+        studente1.setSurname("Occhipinti");
+        studente1.setId(678912);
+
+        // Print new infos
+        System.out.println("New infos:");
+        System.out.println("Name: " + studente1.getName());
+        System.out.println("Surname: " + studente1.getSurname());
+        System.out.println("Id: " + studente1.getId());
+
+    }
 }
